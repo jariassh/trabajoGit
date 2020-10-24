bd2c71e972dc42f96c9eebf679075861011fcb35
Jonathan Arias <informesarias@gmail.com>
2020-10-24 18:30:10 -0500

    Se creó la pagina de contacto y se modulizó con php en la Plantilla.php

---
+++ vistas/paginas/inicio.php
@@ -0,0 +1,6 @@
+<div class="align-items-center minh-100">
+<h1>Bienvenido..!</h1>
+
+<a href="vistas/paginas/contacto.php" class="btn btn-primary mt-2">Ir a Contacto</a>
+    
+</div>
--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -10,54 +10,11 @@
 
 <div class="container-fluid">
     <div class="container">
-        <div class="row">
-            <header class="text-center mb-2 w-100 mt-3" >
-                <h1> Formulario 1</h1>
-                <hr>    
+        <?php
 
-            </header>
+            include "paginas/inicio.php";
 
-        </div>
-        <div class="row">
-            <main class="d-flex justify-content-center w-100" >
-            <div class="col-8">
-                <form class="text-center">
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">Email address</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect1">Example select</label>
-                        <select class="form-control" id="exampleFormControlSelect1">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlSelect2">Example multiple select</label>
-                        <select multiple class="form-control" id="exampleFormControlSelect2">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Example textarea</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                    </div>
-                    <button type="button" class="btn btn-primary">Submit</button>
-                    </form>
-            </div>
-            
-            </main>
-        
-        </div>
-    
+        ?>    
     </div>
 
 </div>
